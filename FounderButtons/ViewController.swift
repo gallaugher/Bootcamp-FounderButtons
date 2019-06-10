bd4b3283325d7a0ba1b29aa67aa4c3bde86b207d
John Gallaugher <john.gallaugher@bc.edu>
2019-06-10 16:20:54 +0100

    FounderButtons from Day 1 of #BCSwiftDublin 2019

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topLabel: UILabel!
+ 
+    @IBOutlet weak var bottomLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func googleButtonPressed(_ sender: UIButton) {
+        topLabel.text = "Larry Page"
+        bottomLabel.text = "Segey Brin"
+    }
+    
+    @IBAction func runwayButtonPressed(_ sender: UIButton) {
+        topLabel.text = "Jennifery Hyman"
+        bottomLabel.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        topLabel.text = ""
+        bottomLabel.text = ""
+    }
+    
 }
 
